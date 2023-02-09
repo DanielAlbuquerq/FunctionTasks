3a87d49157be3f29f26433d26eb822b135d8f625
Daniel Mendes <daniel_developer@hotmail.com>
2023-02-09 13:18:04 -0300

    More range inserted

--- challenges.js
+++ challenges.js
@@ -1,7 +1,9 @@
-//Creat a range function
-function range(a, b, c){
+function range(a, b, c = 1){
+    const n1 = b === undefined ? 1 : a
+    const n2 = b === undefined ? a : b
+    
     const nums = [] 
-    for(let i = 1; i <= a; i++){
+    for(let i = n1; i <= n2; i += c){
       nums.push(i)
     }
     return nums
@@ -9,4 +11,10 @@ function range(a, b, c){
   // range(5) -> [1, 2, 3, 4, 5]
   console.log(range(5))
   
-  // range(6, 11) -> [6,7, 8, 9, 10, 11
+  // range(6, 11) -> [6,7, 8, 9, 10, 11]
+  console.log(range(2, 6))
+
+  //range(10, 19, 2) -> [10, 12, 14, 16,     18]
+  console.log(range(10, 19, 2))
+
+  
